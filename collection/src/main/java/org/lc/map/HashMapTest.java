157ac3535f33c99d812cbf8c13495bf71e7eefa0
chaoliu1025 <chaoliu1025@163.com>
2018-08-17 14:00:51 +0800

    添加 HashMap key为Object时测试。

--- collection/src/main/java/org/lc/map/HashMapTest.java
+++ collection/src/main/java/org/lc/map/HashMapTest.java
@@ -1,8 +1,27 @@
 package org.lc.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class HashMapTest {
 	public static void main(String[] args) {
+		Map<Student, String> map = new HashMap<Student, String>();
+		
+		Student s1 = new Student();
+		s1.setName("张三");
+		Student s2 = new Student();
+		s2.setName("李四");
+		Student s3 = new Student();
+		s3.setName("李四");
+		
+		map.put(s1, "zhang san");
+		map.put(s2, "li si");
+		
+		System.out.println(map.get(s1));
+		System.out.println(map.get(s2));
+		System.out.println(map.get(s3));
 		
 	}
 
